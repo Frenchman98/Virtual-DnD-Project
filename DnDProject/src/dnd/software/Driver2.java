ffc93422c00353f404198c7e32f7c3d392664e4f
alligilbreath <alligilbreath@gmail.com>
2018-11-27 19:55:50 -0700

    Created Game class. Game class and Driver2 are empty except for comments which have some of my ideas of how we could make them

--- DnDProject/src/dnd/software/Driver2.java
+++ DnDProject/src/dnd/software/Driver2.java
@@ -2,4 +2,6 @@ package dnd.software;
 
 public class Driver2 {
 
+	//TODO: We need random dice roll to update the primary and secondary
+	//profiles for player's game character
 }
--- DnDProject/src/dnd/software/Game.java
+++ DnDProject/src/dnd/software/Game.java
@@ -1,13 +1,63 @@
 package dnd.software;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.ArrayList;
+
 
 import dnd.players.*;
 
-public class Game {
+public class Game implements Serializable{
 	private DungeonMaster theDM;
 	private ArrayList<Player> thePlayers;
-	//More to come
+	//TODO: Other attributes needed? Armor? Resources? Weapons?
+	public Game() {
+		theDM = new DungeonMaster();
+		thePlayers = new ArrayList<Player>();
+	}
 	
+	
+	public static Game loadData() {
+		FileInputStream fileIn = null;
+		ObjectInputStream objIn = null;
+		Game aGame = null;
+		
+		try {
+			fileIn = new FileInputStream("Game.ser");
+			objIn = new ObjectInputStream(fileIn);
+			aGame = (Game) objIn.readObject(); //serialized from the object
+			objIn.close();
+			fileIn.close();
+		}
+		catch(IOException ex){
+			ex.printStackTrace();
+			
+		}
+		catch(ClassNotFoundException c){
+			c.printStackTrace(); //print if cannot find the class
+		}
+		
+		return aGame;
+		
+	}
+	
+	public static void saveData(Game aGame) {
+		try 
+			{
+				FileOutputStream fileOut = new FileOutputStream("Game.ser"); //.ser means it's serialized
+				ObjectOutputStream objOut = new ObjectOutputStream(fileOut);
+				objOut.writeObject(aGame);
+				objOut.close();
+				fileOut.close();
+			}
+		catch(IOException ex)
+			{
+				ex.printStackTrace();
+			}
+	}
 
 }
